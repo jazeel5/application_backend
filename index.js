76511bbf67041f0d011d5bc382c46921a6dd4169
Mohammed Jazeel <mohammedjazeel5@gmail.com>
2023-01-31 16:40:33 +0530

    commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const StudentRouter = require('./routes/crud')
 const TeacherRouter = require('./routes/teacher_route')
 const cors = require('cors')
 
-const Student = require('../models/Student')
+// const Student = require('../models/Student')
 
 connectToMongo();
 
@@ -14,25 +14,23 @@ const port = 5001
 
 app.use(express.json());
 
-app.use(cors({
-  origin: "https://application-backend.vercel.app/"
-}))
+app.use(cors())
 
 
 app.use("/test",(req,res)=>{
   res.send("hello world")
 })
 
-app.use("/student", async (req, res) => {
-  const student = await Student.find();
-  try {
-      res.json(student)
-  }
-  catch (error) {
-      console.error(error.message);
-      res.status(500).send("internal some Error occured");
-  }
-})
+// app.use("/student", async (req, res) => {
+//   const student = await Student.find();
+//   try {
+//       res.json(student)
+//   }
+//   catch (error) {
+//       console.error(error.message);
+//       res.status(500).send("internal some Error occured");
+//   }
+// })
 
 app.use('/api/student',StudentRouter)
 
